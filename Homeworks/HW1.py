6cc06eae44156e2fb17bd48b3925462f581904c0
Cemil-Uzun <69428221+Cemil-Uzun@users.noreply.github.com>
2021-03-12 15:53:20 +0300

    Add files via upload

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,8 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+listOdd=[1,15,37,45,69,43]
+listEven=[22,26,24,48,96]
+listMerged=listOdd+listEven
+listMerged=[element * 2 for element in listMerged]
+
+for i in listMerged:
+    print(i, type(i))

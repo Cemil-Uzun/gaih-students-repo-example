1f4a2eed2b11538c61cd6ff26e7d447f9f279f14
Cemil-Uzun <69428221+Cemil-Uzun@users.noreply.github.com>
2021-03-12 18:10:03 +0300

    Add files via upload

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,32 @@
-#Explain your work
+#Getting necessary info from the user
+i=0
+d={}
+for i in range (0,5):
+    student=input("Student Name:")
+    midterm=int(input("Midterm Grade:"))
+    project=int(input("Project Grade:"))
+    final=int(input("Final Grade:"))
+    total=0.3*midterm+0.3*project+0.4*final
+    d[student]=total
+    i +=1
+print(d)
 
-#Question 1
-for x in range(a):
-	print(a)
+#Appending dictionary values (total scores) to list
+list=[]
+for k in d.values():
+    list.append(k)
+
+#Sorting values from higher to lower
+list.sort()
+list=list[::-1]
+print(list)
+
+#Calling keys by values
+listOfKeys=[]
+for x in list:
+    y =[key for (key, value) in d.items() if value == x]
+    listOfKeys.append(y)
+
+#flatten list
+flat_list = [item for sublist in listOfKeys for item in sublist]
+print(flat_list)
